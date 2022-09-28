8495c3001a79c1b700797bc6501f304041eef645
thuctran0912 <tranhuythuc123@gmail.com>
2022-09-28 22:05:24 +0300

    Adding REST

--- src/main/java/com/example/Bookstore/model/web/BookController.java
+++ src/main/java/com/example/Bookstore/model/web/BookController.java
@@ -1,5 +1,8 @@
 package com.example.Bookstore.model.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.model.Book;
 import com.example.Bookstore.model.BookRepository;
@@ -53,6 +57,16 @@ public class BookController {
 		model.addAttribute("categories", categoryRepository.findAll());
 		model.addAttribute("book", repository.findById(id));
 		return "editbook";
-	} 
+	}
+	
+	@GetMapping(value = "/books")
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	@GetMapping(value = "/book/{id}")
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id) {
+		return repository.findById(id);
+	}
 	
 }
